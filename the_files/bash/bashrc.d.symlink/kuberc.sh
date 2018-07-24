9a3a7ca275c0ed66c66ade5691dd282c04d2e3a8
Amit Ramon <amit@riseup.net>
2018-07-24 20:46:59 +0300

    Fixed bash kubectl, added tmux conf

--- the_files/bash/bashrc.d.symlink/kuberc.sh
+++ the_files/bash/bashrc.d.symlink/kuberc.sh
@@ -1,1 +1,5 @@
-. <(kubectl completion bash)
+# if kubectl found, source its bash completion output
+if [ -x /usr/bin/kubectl -o -x /usr/local/bin/kubectl ]; then
+    . <(kubectl completion bash)
+fi
+
