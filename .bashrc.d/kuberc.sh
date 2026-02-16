aedfdbb1611e0daf0b69c089ce12b42572620364
Amit Ramon <amit@riseup.net>
2026-02-16 15:08:47 +0200

    Add kubectl shell resources

--- .bashrc.d/kuberc.sh
+++ .bashrc.d/kuberc.sh
@@ -2,3 +2,8 @@
 if [ -x /usr/bin/kubectl -o -x /usr/local/bin/kubectl ]; then
     . <(kubectl completion bash)
 fi
+
+# ditto minikube
+if [ -x /usr/bin/minikube -o -x /usr/local/bin/minikube ]; then
+    . <(minikube completion bash)
+fi
